ee9879fde2c1e70e563fba52ae163590a84665e3
strzebo <l480r470r1um>
2017-05-16 22:20:24 +0200

    naprawienie zegarów

--- web/WEB-INF/pages/js/czas.js
+++ web/WEB-INF/pages/js/czas.js
@@ -8,6 +8,8 @@ function Czas() {
     var m = minuta * 6;
     var Hm = minuta * 0.5;
 
+    var h;
+
     if (godzina > 12)
         h = (godzina - 12) * 30;
     else
@@ -31,4 +33,80 @@ function Czas() {
 
     setTimeout(Czas, 1000);
 }
-window.onload = Czas;
+
+function CzasNY() {
+    var data = new Date();
+    var sekunda = data.getSeconds();
+    var minuta = data.getMinutes();
+    var godzina = data.getHours() - 6;
+
+    var s = sekunda * 6;
+    var m = minuta * 6;
+    var Hm = minuta * 0.5;
+
+    var h;
+
+    if (godzina > 12)
+        h = (godzina - 12) * 30;
+    else
+        h = godzina * 30;
+
+    var divS = document.getElementById("sekundaNY");
+    divS.style.webkitTransform = "rotate(" + s + "deg)";
+    divS.style.MozTransform = "rotate(" + s + "deg)";
+    divS.style.OTransform = "rotate(" + s + "deg)";
+    divS.style.msTransform = "rotate(" + s + "deg)";
+    var divM = document.getElementById("minutaNY");
+    divM.style.webkitTransform = "rotate(" + m + "deg)";
+    divM.style.MozTransform = "rotate(" + m + "deg)";
+    divM.style.OTransform = "rotate(" + m + "deg)";
+    divM.style.msTransform = "rotate(" + m + "deg)";
+    var divH = document.getElementById("godzinaNY");
+    divH.style.webkitTransform = "rotate(" + (h + Hm) + "deg)";
+    divH.style.MozTransform = "rotate(" + (h + Hm) + "deg)";
+    divH.style.OTransform = "rotate(" + (h + Hm) + "deg)";
+    divH.style.msTransform = "rotate(" + (h + Hm) + "deg)";
+
+    setTimeout(CzasNY, 1000);
+}
+
+function CzasTokio() {
+    var data = new Date();
+    var sekunda = data.getSeconds();
+    var minuta = data.getMinutes();
+    var godzina = data.getHours() + 6;
+
+    var s = sekunda * 6;
+    var m = minuta * 6;
+    var Hm = minuta * 0.5;
+
+    if (godzina > 12)
+        h = (godzina - 12) * 30;
+    else
+        h = godzina * 30;
+
+    var divS = document.getElementById("sekundaTokio");
+    divS.style.webkitTransform = "rotate(" + s + "deg)";
+    divS.style.MozTransform = "rotate(" + s + "deg)";
+    divS.style.OTransform = "rotate(" + s + "deg)";
+    divS.style.msTransform = "rotate(" + s + "deg)";
+    var divM = document.getElementById("minutaTokio");
+    divM.style.webkitTransform = "rotate(" + m + "deg)";
+    divM.style.MozTransform = "rotate(" + m + "deg)";
+    divM.style.OTransform = "rotate(" + m + "deg)";
+    divM.style.msTransform = "rotate(" + m + "deg)";
+    var divH = document.getElementById("godzinaTokio");
+    divH.style.webkitTransform = "rotate(" + (h + Hm) + "deg)";
+    divH.style.MozTransform = "rotate(" + (h + Hm) + "deg)";
+    divH.style.OTransform = "rotate(" + (h + Hm) + "deg)";
+    divH.style.msTransform = "rotate(" + (h + Hm) + "deg)";
+
+    setTimeout(CzasTokio, 1000);
+}
+window.addEventListener("load", function () {
+
+    console.log("test");
+    Czas();
+    CzasNY();
+    CzasTokio();
+});
